13c1fa312622bff382f8a28344d0fbcb50af2b62
Nano2211 <drmnene@gmail.com>
2024-04-02 14:27:00 -0700

    owari

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,8 @@
 #!/bin/usr/python3
-
 """Define a class Square"""
 
 class Square:
     """REpresents square"""
-
     def __init__(self, size=0):
         """init new square.
         Args:
@@ -14,10 +12,19 @@ class Square:
 
     @property
     def size(self):
+        """integer: private size.
+        Returns:
+        Private size.
+        """
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """Sets value into size which must be an int.
+
+        Args:
+            value(int): size of square.
+            """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -26,4 +33,4 @@ class Square:
 
         def area(self):
             """Return area"""
-            return (self.__size * self.__size)
+            return (self.__size ** 2)
